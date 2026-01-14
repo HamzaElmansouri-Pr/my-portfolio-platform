70be0c3b4d39326015e2d2ea676575e9676d32ae
hamza <hamza.elmansouri.e@gmail.com>
2026-01-14 19:29:43 +0100

    feat(init): scaffold monorepo structure for spring boot and next.js

---
+++ backend/src/test/java/com/hamza/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamza.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
